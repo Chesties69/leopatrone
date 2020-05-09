a6821ac093eec165fe97e38d5d5d0444dc8044fb
Dane Hansen <dane@danehansen.com>
2020-05-08 21:48:39 -0700

    make colors on home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from './Home.module.scss';
+import { random} from '@danehansen/math';
 
 export default function Home() {
+  const blocks = []
+  for(let i = 0; i < 1000; i++) {
+    blocks.push(<div className={styles.block} style={{backgroundColor: `rgba(${random(0,255,true)},${random(0,255,true)},${random(0,255,true)},1)`}} key={i}/>)
+  }
+
   return (
-    <div>
-      <h1>home page</h1>
+    <div className={styles.root}>
+      {blocks}
     </div>
   );
 }
